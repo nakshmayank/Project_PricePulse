9b2a141bded0bd133b39c298835e655aa5703f86
nakshmayank <nakshmayank2000@gmail.com>
2026-03-31 17:48:43 +0530

    more chnages

--- ml-service/app.py
+++ ml-service/app.py
@@ -45,5 +45,8 @@ def predict():
         "predictedDemand": round(float(demand_score), 2)
     })
 
+# if __name__ == '__main__':
+#     app.run(port=5001, debug=True)
+
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    app.run(debug=True)
